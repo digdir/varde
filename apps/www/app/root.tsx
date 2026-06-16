308077080fbf59765317a9347e2548137a1b9693
Barsnes <tobias.barsnes@digdir.no>
2026-06-16 12:49:40 +0200

    rename files, logo

--- apps/www/app/root.tsx
+++ apps/www/app/root.tsx
@@ -28,7 +28,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='no' data-color-scheme='auto' suppressHydrationWarning>
+    <html lang='no' data-color-scheme='light' suppressHydrationWarning>
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
